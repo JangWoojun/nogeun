b0d6222e6a22a05f94880890eadac4e0e16d4e5f
JangWoojun <woojun0107@naver.com>
2024-01-27 17:39:52 +0900

    feat: 카카오 로그인 로직 구현

---
+++ app/src/main/java/com/example/ignis/GlobalApplication.kt
@@ -0,0 +1,14 @@
+package com.example.ignis
+
+import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
+
+class GlobalApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // 다른 초기화 코드들
+
+        // Kakao SDK 초기화
+        KakaoSdk.init(this, "c8173cd37ae24858bdc1a57832061386")
+    }
+}
---
+++ app/src/main/java/com/example/ignis/LoginActivity.kt
@@ -0,0 +1,83 @@
+package com.example.ignis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.ignis.databinding.ActivityLoginBinding
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    companion object {
+        const val TAG = "확인"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            UserApiClient.instance.me { user, error ->
+                if (error != null) {
+                    Log.e(TAG, "사용자 정보 요청 실패", error)
+                }
+                else if (user != null) {
+                    Log.i(TAG, "사용자 정보 요청 성공" +
+                            "\n회원번호: ${user.id}" +
+                            "\n이메일: ${user.kakaoAccount?.email}" +
+                            "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
+                            "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}")
+                }
+            }
+
+            val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+                if (error != null) {
+                    Log.e(TAG, "카카오계정으로 로그인 실패", error)
+                } else if (token != null) {
+                    Log.i(TAG, "카카오계정으로 로그인 성공 ${token.accessToken}")
+                }
+            }
+
+            login.setOnClickListener {
+                if (UserApiClient.instance.isKakaoTalkLoginAvailable(this@LoginActivity)) {
+                    UserApiClient.instance.loginWithKakaoTalk(this@LoginActivity) { token, error ->
+                        if (error != null) {
+                            Log.e(TAG, "카카오톡으로 로그인 실패", error)
+
+                            // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                            // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                            if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                                return@loginWithKakaoTalk
+                            }
+
+                            // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                            UserApiClient.instance.loginWithKakaoAccount(this@LoginActivity, callback = callback)
+                        } else if (token != null) {
+                            Log.i(TAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
+                            UserApiClient.instance.me { user, error ->
+                                if (error != null) {
+                                    Log.e(TAG, "사용자 정보 요청 실패", error)
+                                }
+                                else if (user != null) {
+                                    Log.i(TAG,
+                                        "사용자 정보 요청 성공" +
+                                        "\n회원번호: ${user.id}" +
+                                        "\n이메일: ${user.kakaoAccount?.email}" +
+                                        "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
+                                        "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}"
+                                    )
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    UserApiClient.instance.loginWithKakaoAccount(this@LoginActivity, callback = callback)
+                }
+            }
+        }
+    }
+}
