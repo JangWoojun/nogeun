b7261d7ae614209e79ea0b9a209e3623b93abae6
JangWoojun <woojun0107@naver.com>
2024-01-27 19:44:38 +0900

    feat: 지도 화면 세팅

--- app/src/main/java/com/example/ignis/MainActivity.kt
+++ app/src/main/java/com/example/ignis/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.ignis
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ignis.databinding.ActivityMainBinding
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.MapLifeCycleCallback
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        const val TAG = "확인"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            mapView.start(object : MapLifeCycleCallback() {
+                override fun onMapDestroy() {
+                    // 지도 API 가 정상적으로 종료될 때 호출됨
+                    Log.d(TAG, "정상 종료")
+                }
+
+                override fun onMapError(error: Exception) {
+                    Log.d(TAG, error.message.toString())
+                }
+            }, object : KakaoMapReadyCallback() {
+                override fun onMapReady(kakaoMap: KakaoMap) {
+                    Log.d(TAG, "정상 시작")
+                }
+            })
+        }
     }
 }
