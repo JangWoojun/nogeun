82105ec8bdb6ddce65b63bdc21ace02d15098104
JangWoojun <woojun0107@naver.com>
2024-01-27 21:50:29 +0900

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,7 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
     implementation("de.hdodenhof:circleimageview:3.1.0")
+
+    implementation("com.google.android.gms:play-services-location:21.1.0")
+
 }
--- app/src/main/java/com/example/ignis/MainActivity.kt
+++ app/src/main/java/com/example/ignis/MainActivity.kt
@@ -1,15 +1,34 @@
 package com.example.ignis
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Color
 import android.location.Location
-import android.location.LocationRequest
+import com.google.android.gms.location.LocationRequest
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.ignis.databinding.ActivityMainBinding
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import com.kakao.vectormap.KakaoMap
 import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.LatLng
 import com.kakao.vectormap.MapLifeCycleCallback
+import com.kakao.vectormap.camera.CameraUpdateFactory
+import com.kakao.vectormap.label.LabelOptions
+import com.kakao.vectormap.label.LabelStyle
+import com.kakao.vectormap.label.LabelStyles
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,8 +38,6 @@ class MainActivity : AppCompatActivity() {
     private val REQUEST_PERMISSION_LOCATION = 10
     private var kakaoMap: KakaoMap? = null
     private var myLocation: Location? = null
-    private var isTracking = false
-
     companion object {
         const val TAG = "확인"
     }
@@ -30,20 +47,85 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.apply {
+
+            mLocationRequest =  LocationRequest.create().apply {
+                priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            }
+
             mapView.start(object : MapLifeCycleCallback() {
                 override fun onMapDestroy() {
-                    // 지도 API 가 정상적으로 종료될 때 호출됨
-                    Log.d(TAG, "정상 종료")
                 }
 
                 override fun onMapError(error: Exception) {
-                    Log.d(TAG, error.message.toString())
                 }
-            }, object : KakaoMapReadyCallback() {
-                override fun onMapReady(kakaoMap: KakaoMap) {
-                    Log.d(TAG, "정상 시작")
-                }
-            })
+            },
+                object : KakaoMapReadyCallback() {
+                    override fun onMapReady(kakaoMap: KakaoMap) {
+                        this@MainActivity.kakaoMap = kakaoMap
+                        if (checkPermissionForLocation(this@MainActivity)) {
+                            startLocationUpdates()
+                        }
+                    }
+
+                    override fun getZoomLevel(): Int {
+                        return 15
+                    }
+                })
+
         }
     }
+    private fun startLocationUpdates() {
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+        mFusedLocationProviderClient!!.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper())
+    }
+
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            onLocationChanged(locationResult.lastLocation!!)
+        }
+    }
+
+    fun onLocationChanged(location: Location) {
+        val cameraUpdate = CameraUpdateFactory.newCenterPosition(LatLng.from(location.latitude, location.longitude))
+        kakaoMap!!.moveCamera(cameraUpdate)
+
+        myLocation = location
+
+        val styles = kakaoMap!!.labelManager!!
+            .addLabelStyles(LabelStyles.from(LabelStyle.from(R.drawable.my_location)))
+        val options: LabelOptions =
+            LabelOptions.from(LatLng.from(location.latitude, location.longitude)).setStyles(styles)
+        val layer = kakaoMap!!.labelManager!!.layer
+        val label = layer!!.addLabel(options)
+
+        label.show()
+
+    }
+
+
+    private fun checkPermissionForLocation(context: Context): Boolean {
+        return if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            true
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_PERMISSION_LOCATION)
+            false
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_PERMISSION_LOCATION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationUpdates()
+            } else {
+                Toast.makeText(this, "권한이 없어 지도를 사용할 수 없습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
 }
