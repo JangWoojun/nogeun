263b5d35aa88272f7c60adfe9ff2d1d7d5b6c1b4
JangWoojun <woojun0107@naver.com>
2024-01-27 19:44:14 +0900

    feat: kakaomap 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,5 +48,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("com.kakao.sdk:v2-user:2.19.0") // 카카오 로그인
+    implementation("com.kakao.sdk:v2-user:2.19.0") // 카카오 로그인
+    implementation("com.kakao.maps.open:android:2.6.3")
+
 }
