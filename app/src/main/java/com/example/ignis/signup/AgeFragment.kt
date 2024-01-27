3a71b80dd7a7ae1078e9bf105bb87e57e835230b
JangWoojun <woojun0107@naver.com>
2024-01-28 08:39:01 +0900

    .

--- app/src/main/java/com/example/ignis/signup/AgeFragment.kt
+++ app/src/main/java/com/example/ignis/signup/AgeFragment.kt
@@ -1,10 +1,15 @@
 package com.example.ignis.signup
 
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.example.ignis.R
 import com.example.ignis.databinding.FragmentAgeBinding
 import com.example.ignis.main.MainActivity
 
@@ -40,10 +45,35 @@ class AgeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
+            input.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(charSequence: CharSequence?, start: Int, count: Int, after: Int) {
+                    // 텍스트 변경 전에 호출
+                }
+
+                override fun onTextChanged(charSequence: CharSequence?, start: Int, before: Int, count: Int) {
+                    if (input.text.isNotEmpty()) {
+                        val newBackgroundColor = ContextCompat.getColor(requireContext(),
+                            R.color.primary
+                        )
+                        button.backgroundTintList = ColorStateList.valueOf(newBackgroundColor)
+                    } else {
+                        val newBackgroundColor = ContextCompat.getColor(requireContext(),
+                            R.color.gray500
+                        )
+                        button.backgroundTintList = ColorStateList.valueOf(newBackgroundColor)
+                    }
+                }
+
+                override fun afterTextChanged(editable: Editable?) {
+                }
+            })
+
             button.setOnClickListener {
-                val data = arguments?.getString(ARG_DATA)
-                val response = SignupRequest(input.text.toString().toInt(), data.toString())
-                (activity as? SignUpActivity)?.onButtonAgeClicked(response)
+                if (input.text.isNotEmpty()) {
+                    val data = arguments?.getString(ARG_DATA)
+                    val response = SignupRequest(input.text.toString().toInt(), data.toString())
+                    (activity as? SignUpActivity)?.onButtonAgeClicked(response)
+                }
             }
         }
     }
--- app/src/main/java/com/example/ignis/signup/NameFragment.kt
+++ app/src/main/java/com/example/ignis/signup/NameFragment.kt
@@ -1,10 +1,15 @@
 package com.example.ignis.signup
 
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.example.ignis.R
 import com.example.ignis.databinding.FragmentNameBinding
 
 class NameFragment : Fragment() {
@@ -35,9 +40,34 @@ class NameFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
             button.setOnClickListener {
-                val data = input.text.toString()
-                listener.onButtonNameClicked(data)
+                if (input.text.isNotEmpty()) {
+                    val data = input.text.toString()
+                    listener.onButtonNameClicked(data)
+                }
             }
+
+            input.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(charSequence: CharSequence?, start: Int, count: Int, after: Int) {
+                    // 텍스트 변경 전에 호출
+                }
+
+                override fun onTextChanged(charSequence: CharSequence?, start: Int, before: Int, count: Int) {
+                    if (input.text.isNotEmpty()) {
+                        val newBackgroundColor = ContextCompat.getColor(requireContext(),
+                            R.color.primary
+                        )
+                        button.backgroundTintList = ColorStateList.valueOf(newBackgroundColor)
+                    } else {
+                        val newBackgroundColor = ContextCompat.getColor(requireContext(),
+                            R.color.gray500
+                        )
+                        button.backgroundTintList = ColorStateList.valueOf(newBackgroundColor)
+                    }
+                }
+
+                override fun afterTextChanged(editable: Editable?) {
+                }
+            })
         }
 
     }
