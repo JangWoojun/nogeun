a8d909cfa067a5d046cf4669868a3c2f9f8f5aed
JangWoojun <woojun0107@naver.com>
2024-01-27 20:54:11 +0900

    .

--- app/src/main/java/com/example/ignis/MainActivity.kt
+++ app/src/main/java/com/example/ignis/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.ignis
 
+import android.location.Location
+import android.location.LocationRequest
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ignis.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.kakao.vectormap.KakaoMap
 import com.kakao.vectormap.KakaoMapReadyCallback
 import com.kakao.vectormap.MapLifeCycleCallback
@@ -11,6 +14,12 @@ import com.kakao.vectormap.MapLifeCycleCallback
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var mFusedLocationProviderClient: FusedLocationProviderClient? = null
+    private lateinit var mLocationRequest: LocationRequest
+    private val REQUEST_PERMISSION_LOCATION = 10
+    private var kakaoMap: KakaoMap? = null
+    private var myLocation: Location? = null
+    private var isTracking = false
 
     companion object {
         const val TAG = "확인"
