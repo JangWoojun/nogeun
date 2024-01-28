062767b793cf5aee63a053d248153069adb2dcd1
uiel <parkuiery@gmail.com>
2024-01-28 11:32:35 +0900

    .

--- app/src/main/java/com/example/ignis/profile/ProfileAdapter.kt
+++ app/src/main/java/com/example/ignis/profile/ProfileAdapter.kt
@@ -1,16 +1,20 @@
 package com.example.ignis.profile
 
 import android.content.Context
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.ignis.R
 import com.example.ignis.userInfo.Feed
 import de.hdodenhof.circleimageview.CircleImageView
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class ProfileAdapter(private val itemList: List<Feed>, private val context: Context) :
 RecyclerView.Adapter<ProfileAdapter.ProfileViewHolder>() {
@@ -23,11 +27,19 @@ RecyclerView.Adapter<ProfileAdapter.ProfileViewHolder>() {
         return ProfileViewHolder(view)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: ProfileAdapter.ProfileViewHolder, position: Int) {
         itemList[position].apply {
+            Glide
+                .with(context)
+                .load(image_url)
+                .into(holder.img)
+            val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
+            val parsedDateTime = LocalDateTime.parse(created_date, formatter)
+            val formattedDate = parsedDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
             holder.title.text = title
             holder.name.text = author_name
-            holder.date.text = created_date
+            holder.date.text = formattedDate
             holder.like.text = "$likes 개"
         }
     }
@@ -37,6 +49,7 @@ RecyclerView.Adapter<ProfileAdapter.ProfileViewHolder>() {
     }
 
     inner class ProfileViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val img: ImageView = view.findViewById(R.id.iv_profile_item)
         val title: TextView = view.findViewById(R.id.tv_profile_item_title)
         val name: TextView = view.findViewById(R.id.tv_profile_item_name)
         val date: TextView = view.findViewById(R.id.tv_profile_item_date)
